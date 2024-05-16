6b78fcd447f1b7a05fc89e11fa27ee10eb45d270
Ramitha Heshan <142712365+RamithaHeshan33@users.noreply.github.com>
2024-05-16 13:37:05 +0530

    Update login.php

--- login.php
+++ login.php
@@ -3,23 +3,16 @@ session_start();
 include_once('connection.php');
 
 if (isset($_POST['login'])) {
-
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    if (empty($username) && empty($password)) {
-        echo "<script>alert('Please Fill Username and Password'); window.location='index.php';</script>";
+    if (empty($username) || empty($password)) {
+        $message = 'Please Fill ';
+        if (empty($username)) $message .= 'Username ';
+        if (empty($password)) $message .= 'Password ';
+        echo "<script>alert('$message'); window.location='index.php';</script>";
         exit;
-    }
-    elseif (empty($password)) {
-        echo "<script>alert('Please Fill Password'); window.location='index.php';</script>";
-        exit;
-    }
-    elseif (empty($username)) {
-        echo "<script>alert('Please Fill Username'); window.location='index.php';</script>";
-        exit;
-    }
-    else {
+    } else {
         // Use prepared statements to prevent SQL injection
         $stmt = $conn->prepare("SELECT * FROM `login_tbl` WHERE `username` = ?");
         $stmt->bind_param("s", $username);
@@ -29,7 +22,7 @@ if (isset($_POST['login'])) {
         if ($result->num_rows > 0) {
             $row = $result->fetch_assoc();
             $storedUsername = $row['username'];
-            $storedPassword = $row['password'];
+            $storedPassword = $row['password']; // Assume this is hashed
 
             // Use password_verify to check hashed password
             if (password_verify($password, $storedPassword)) {
